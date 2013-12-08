d6aab06e3b06bfe2a8508234459ec158f905bf0d
loganbfisher <logan@cedi.com>
2013-12-08 13:00:07 -0600

    deleted public assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,16 @@
 //= require_tree .
 //= require_self
 $(function(){ $(document).foundation(); });
+
+var random_color = function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 9)];
+    }
+    return color;
+}
+
+$('span.label').each(function(){
+    $(this).css('background', random_color);
+});
